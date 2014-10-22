9090955a395209a2c0d720b43f5ce21af2d6c5dd
Tim Licata <tim@timothylicata.com>
2014-10-21 22:33:44 -0700

    page for showing a tag and its posts

--- app.js
+++ app.js
@@ -64,4 +64,13 @@ app.get("/authors/:id", function (req, res) {
   });
 });
 
+app.get("/tags/:name", function (req, res) {
+  var tagName = req.params.name;
+  db.Tag.find({where: {name: tagName}}).done(function (err, tag) {
+    tag.getPosts().done(function (err, posts) {
+      res.render("tags/show.ejs", {tag:tag, posts:posts});
+    });
+  });
+});
+
 app.listen(3000);
